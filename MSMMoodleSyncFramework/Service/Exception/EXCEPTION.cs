e3d74b9dbb6eff2101d390813a7e8449f22732f4
Murugan <Murugan@MURUGAN-PC>
2016-04-09 23:23:07 +0400

    Added exception

---
+++ MSMMoodleSyncFramework/Service/Exception/EXCEPTION.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace MSMMoodleSyncFramework.Service.Exception
+{
+
+    [XmlRoot(ElementName = "EXCEPTION")]
+    public class EXCEPTION
+    {
+
+        [XmlElement(ElementName = "MESSAGE")]
+        public string MESSAGE { get; set; }
+
+        [XmlElement(ElementName = "DEBUGINFO")]
+        public string DEBUGINFO { get; set; }
+
+        [XmlAttribute(AttributeName = "class")]
+        public string Class { get; set; }
+
+    }
+
+}
